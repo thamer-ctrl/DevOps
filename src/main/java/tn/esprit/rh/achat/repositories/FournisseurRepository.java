37db254896dd650912b38a0eeeea05f115bb7aef
thamer-ctrl <thamer.barhoumi@esprit.tn>
2022-11-05 17:06:54 -0700

    comment branch thamer

--- src/main/java/tn/esprit/rh/achat/repositories/FournisseurRepository.java
+++ src/main/java/tn/esprit/rh/achat/repositories/FournisseurRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tn.esprit.rh.achat.entities.Fournisseur;
 
+// Comment for branch_thamer
 @Repository
 public interface FournisseurRepository extends JpaRepository<Fournisseur, Long> {
 
