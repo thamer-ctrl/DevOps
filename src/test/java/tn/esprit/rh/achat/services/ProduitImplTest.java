61aaa7a2d3a429c5372fadaee3e5fd42e4cd2759
Hajer.SAIDI <hajer.saidi@esprit.tn>
2022-11-11 18:58:11 +0100

    update commit2

--- src/test/java/tn/esprit/rh/achat/services/ProduitImplTest.java
+++ src/test/java/tn/esprit/rh/achat/services/ProduitImplTest.java
@@ -1,7 +1,7 @@
 package tn.esprit.rh.achat.services;
 
 import org.junit.jupiter.api.MethodOrderer;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
